070519054216104249fad38a68617e389f6441fe
Bogdan Stefanyuk <86630295+Ponimon4ik@users.noreply.github.com>
2022-05-17 16:34:03 +0300

    Исправил замечания flake8

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,15 +1,6 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
 import csv
 import datetime as dt
 from collections import defaultdict
-
-from itemadapter import ItemAdapter
 from pathlib import Path
 
 BASE_DIR = Path(__file__).parent.parent
